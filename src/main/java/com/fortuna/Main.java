20dfc1885dcc932d1a7abf6174e86b54ed1ba475
lai2-24 <lai2-24@itismeucci.com>
2025-10-08 10:01:03 +0200

    working calculator

--- src/main/java/com/fortuna/Main.java
+++ src/main/java/com/fortuna/Main.java
@@ -1,7 +1,44 @@
 package com.fortuna;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        System.out.println("Client avviato");
+
+        System.out.println("Inserisci ip del server");
+        Scanner scan = new Scanner(System.in);
+
+        String ip = scan.nextLine();
+        System.out.println("Inserisci porta del server");
+        int port = Integer.parseInt(scan.nextLine());
+
+        Socket mioSocket = new Socket(ip, port);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(mioSocket.getOutputStream(), true);
+
+        System.out.println(in.readLine());
+        out.println("Versione client: 0.9");
+
+        System.out.println("Inserisci primo numero");
+        out.println(scan.nextLine());
+
+        System.out.println("Inserisci secondo numero");
+        out.println(scan.nextLine());
+
+        System.out.println("Inserisci operazione (1=+,2=-,3=/,4=*)");
+        out.println(scan.nextLine());
+
+        System.out.println("Risultato: " + in.readLine());
+
+        mioSocket.close();
+        scan.close();
     }
 }
